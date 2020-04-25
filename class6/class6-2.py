3af604777b0be54e103bf84b9b663553b018add8
BuildTools <unconfigured@null.spigotmc.org>
2020-04-25 15:19:10 +0800

    class6

--- class6/class6-2.py
+++ class6/class6-2.py
@@ -33,12 +33,27 @@ class AfghanHound(Dog):
      
     def roar(self):
         print("喔喔喔")
-
+class Cat(Animal):
+    def __init__(self,HP):
+        super().__init__(0.5)
+        
+    def meaw(self):
+        print("喵喵喵")
+        
+class Smallcat(Animal):
+    def __init__(self,HP):
+        super().__init__(0.25)
+        
+    def meaw(self):
+        print("喵喵喵")
 dog1 = AfghanHound()
 dog2 = Affenpinscher()
-
+cat1 = Cat()
+cat2 = Smallcat()
 dog2.roar()
 dog1.roar()
+cat2.roar()
+cat1.roar()
 
         
         
